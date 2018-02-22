2bc3df073ec7b60664648f47653f479bf79c5159
jimmygls <jimmygls@gmail.com>
2018-02-22 10:45:53 -0500

    eventos y directivas

--- src/app/empleados/empleados.component.ts
+++ src/app/empleados/empleados.component.ts
@@ -11,21 +11,26 @@ export class EmpleadosComponent implements OnInit {
   public tituloEmpleado = 'Componente Empleados:';
   public empleado: Empleado;
   public trabajadores: Array<Empleado>;
+  public trabajador_externo: boolean;
 
   constructor() {
     this.empleado = new Empleado('Jimmy', 37, 'Desa', true);
     this.trabajadores = [
       new Empleado('Jimmy', 37, 'Desa', true),
-      new Empleado('Pedro', 35, 'Prue', true),
+      new Empleado('Pedro', 35, 'Prue', false),
       new Empleado('Marco', 27, 'Lid', true),
       new Empleado('Jose', 47, 'Adm', true)
     ];
-
+    this.trabajador_externo = true;
   }
 
   ngOnInit() {
     console.log(this.empleado);
     console.log(this.trabajadores);
+  }
+
+  cambiarTrabajadorExterno(valor) {
+    this.trabajador_externo = valor;
   }
 
 }
