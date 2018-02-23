da1924a5c76bb364723750e248084904388ab5fa
jimmygls <jimmygls@gmail.com>
2018-02-23 11:40:52 -0500

    ejercicio con los servicios

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,13 +9,24 @@ import { RopaService } from '../servicios/ropa.service';
 })
 export class HomeComponent implements OnInit {
   public titulo = 'Pagina Principal'
-  
+  public listado_ropa:Array<string>;
+  public prenda_a_guardar:string;
   //se inyecta el servicio
   constructor(private _ropaService: RopaService) {
   }
 
   ngOnInit() {
-    console.log(this._ropaService.prueba('camiseta nike'));
+    this.listado_ropa=this._ropaService.getRopa();
+    console.log(this.listado_ropa);
+  }
+
+  guardarPrenda(){
+    this._ropaService.addRopa(this.prenda_a_guardar);
+    this.prenda_a_guardar=null;
+  }
+
+  eliminarPrenda(indice:number){
+    this._ropaService.removeRopa(indice);
   }
 
 }
--- src/app/servicios/ropa.service.ts
+++ src/app/servicios/ropa.service.ts
@@ -2,12 +2,26 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class RopaService {
-  
-  public nombrePrenda = 'Pantalones vaqueros';
-  
+
+  public nombre_Prenda = 'Pantalones vaqueros';
+  public colleccion_ropa = ['Pantalones blanco', 'Camiseta roja'];
   constructor() { }
-  
+
   prueba(nombre_Prenda) {
     return nombre_Prenda;
+  }
+
+  addRopa(nombre_Prenda: string): Array<string> {
+    this.colleccion_ropa.push(nombre_Prenda);
+    return this.getRopa();
+  }
+
+  getRopa():Array<string> {
+    return this.colleccion_ropa;
+  }
+
+  removeRopa(indice:number):Array<string>{
+    this.colleccion_ropa.splice(indice,1);
+    return this.getRopa();
   }
 }
