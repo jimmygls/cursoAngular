225f28210958f655e509a6b52d23f5f21856e9c8
jimmygls <jimmygls@gmail.com>
2018-04-02 14:49:20 -0500

    Parte uno formularios

--- src/app/routing-app/routing-app-routing.module.ts
+++ src/app/routing-app/routing-app-routing.module.ts
@@ -5,6 +5,7 @@ import { EmpleadosComponent } from '../empleados/empleados.component';
 import { FrutaComponent } from '../fruta/fruta.component';
 import { HomeComponent } from '../home/home.component';
 import { ContactoComponent } from '../contacto/contacto.component';
+import { CochesComponent } from '../coches/coches.component';
 
 export const appRoutes: Routes = [
   {path:'',component:HomeComponent},//este es home
@@ -13,6 +14,7 @@ export const appRoutes: Routes = [
   {path:'empleado',component:EmpleadosComponent},//ruta de empleado
   {path:'contacto',component:ContactoComponent},//ruta de contacto  
   {path:'contacto/:page',component:ContactoComponent},//ruta de contacto que recibe un parametro 
+  {path:'coches',component:CochesComponent},//ruta para coches
   {path:'**',component:HomeComponent}//esta es la pagina cuando se produce un error
 ];
 
