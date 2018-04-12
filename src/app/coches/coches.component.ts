910f148c6f5820c5c7eaa3a69dba7f9871137e4f
jimmygls <jimmygls@gmail.com>
2018-04-12 15:41:17 -0500

    Valiadacion y eventos en el formulario

--- src/app/coches/coches.component.ts
+++ src/app/coches/coches.component.ts
@@ -9,10 +9,17 @@ import { Coche } from './coche';
 export class CochesComponent implements OnInit {
 
   public coche: Coche;
+  public coches:Array<Coche>;
   constructor() {
     this.coche = new Coche("", 0, "");
+    this.coches=[
+      new Coche("Seat Panda", 120, "Blanco"),
+      new Coche("Renault Clio", 110, "Azul"),
+    ];
   }
   onSubmit() {
+    this.coches.push(this.coche);
+    this.coche= new Coche("", 0, "");
     console.log(this.coche);
   }
   ngOnInit() {
